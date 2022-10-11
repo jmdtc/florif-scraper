805c3b77078efd01598e301beef68d9828ffd711
Mathieu Delisle <mathieudelisle@outlook.com>
2022-10-11 13:45:05 +0200

    works?

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
 const { chromium } = require("playwright");
 const XLSX = require("xlsx");
+const fs = require("fs").promises;
+const { parse } = require("csv-parse/sync");
 
-const plants = [
-  "Acer campestre",
-  "Acer negundo",
-  "Ribes alpinum",
-  "Ribes rubrum",
-];
-
-function chunkify(arr, size) {
+function chunkify(arr, size, fillLast = false) {
   let result = [];
   let temp = [];
   for (const [idx, el] of Object.entries(arr)) {
@@ -19,9 +14,12 @@ function chunkify(arr, size) {
     }
   }
 
-  const last = result[result.length - 1];
-  while (last.length < 3) {
-    last.push("Ribes nigrum");
+  if (fillLast) {
+    const last = result[result.length - 1];
+    while (last.length < 3) {
+      // manoucherie
+      last.push("Ribes nigrum");
+    }
   }
 
   return result;
@@ -77,9 +75,7 @@ async function getPlantsInformation(chunk, page) {
   return formatCellsContent(content, activePlants);
 }
 
-function exportToExcel(data) {
-  const fileName = "florif.xlsx";
-
+function exportToExcel(data, fileName = "florif.xlsx") {
   const ws = XLSX.utils.json_to_sheet(data);
   const wb = XLSX.utils.book_new();
   XLSX.utils.book_append_sheet(wb, ws, "florif");
@@ -87,8 +83,82 @@ function exportToExcel(data) {
   XLSX.writeFile(wb, fileName);
 }
 
-async function main() {
-  const chunks = chunkify(plants, 3);
+// pour import les plantes de ton drive
+async function importPlants() {
+  const buffer = await fs.readFile("./reeeee.csv");
+  const data = parse(buffer);
+
+  let result = [];
+
+  // syntaxe pour plantName = data[idx][0]
+  for (const [plantName] of data) {
+    if (plantName.includes("subsp.") || plantName.includes("var.")) continue;
+    const nameCleaned = plantName.split(" ").slice(0, 2).join(" ");
+    result.push(nameCleaned);
+  }
+
+  return result.slice(1, result.length);
+}
+
+async function doesPlantExist(page, plantName) {
+  let result = true;
+
+  await page
+    .locator(`.l-29 >> text=${plantName}`)
+    .textContent()
+    .catch((_) => (result = false));
+
+  return { [plantName]: result };
+}
+
+async function getChunkInformation(page, chunk) {
+  const input = page.locator("#A6");
+  const button = page.locator("#A13");
+
+  for (const plant of chunk) {
+    await input.fill(plant);
+    await button.click();
+  }
+
+  const result = await Promise.all(
+    chunk.map((plant) => doesPlantExist(page, plant))
+  );
+
+  return result;
+}
+
+async function reloadComparePage(page) {
+  await page.goto(
+    "http://www.florif.fr/SITE_FLORIF/PAGE_SD_Comparaison/8BQAABdOfsZXbWxxcndGTVRvBgA"
+  );
+}
+
+function exportPlants(parsed, browser) {
+  const plantsWhichExist = parsed
+    .filter((plant) => Object.values(plant)[0])
+    .map((plant) => ({ name: Object.keys(plant)[0] }));
+
+  exportToExcel(plantsWhichExist, `lol-test.xlsx`);
+  browser.close();
+}
+
+function resumeScrapingFromExcel(filePath, chunks) {
+  const read = XLSX.readFile(filePath);
+  const data = XLSX.utils.sheet_to_json(read.Sheets.florif);
+
+  const lastName = data.slice(-2)[0].name;
+
+  const lastChunkIdx = chunks.findIndex((c) =>
+    c.some((p) => p.includes(lastName))
+  );
+
+  return lastChunkIdx || 0;
+}
+
+// c'était pour tej les mauvais noms
+async function getCleanPlantData() {
+  const plantNames = await importPlants();
+  const chunks = chunkify(plantNames, 3);
 
   const browser = await chromium.launch({
     headless: true,
@@ -97,18 +167,61 @@ async function main() {
   const page = await browser.newPage();
 
   let result = [];
-  for (const chunk of chunks) {
-    // faut changer l'url de temps en temps pcq elle est dynamique cheloue
-    await page.goto(
-      "http://www.florif.fr/SITE_FLORIF/PAGE_SD_Comparaison/IyIAAGGV9Yh1Qk1ldXlzZ3RnAAA?A5"
-    );
+  let chunkNumber = 1;
+  let flag = false;
+  for (const [chunkIdx, chunk] of chunks.entries()) {
+    await reloadComparePage(page).catch((_) => {
+      exportPlants(result, browser);
+      flag = true;
+    });
 
-    const plants = await getPlantsInformation(chunk, page);
-    result = [...result, ...plants];
+    if (flag) return;
+
+    const chunkInformation = await getChunkInformation(page, chunk);
+    result = result.concat(chunkInformation);
+    console.log((chunkNumber / chunks.length) * 100);
+    chunkNumber++;
   }
 
+  exportPlants(result, browser);
+}
+
+async function importPlantsFinal() {
+  const buffer = await fs.readFile("./final.csv");
+  //ptit problème d'encodage du csv il a ajouté des ;;; à la fin et le nom de la colonne
+  const plantsNames = parse(buffer).map((subArr) => subArr[0].split(";")[0]);
+  return plantsNames.slice(1, plantsNames.length);
+}
+
+async function finalScraper({ page, path, chunk }) {
+  const url = `http://www.florif.fr/SITE_FLORIF/PAGE_SD_Comparaison/${path}`;
+  await page.goto(url);
+
+  return getPlantsInformation(chunk, page);
+}
+
+async function main() {
+  const plants = await importPlantsFinal();
+  const chunks = chunkify(plants, 3, true);
+
+  const browser = await chromium.launch({
+    headless: true,
+  });
+  const page = await browser.newPage();
+
+  let result = [];
+  for (const chunk of chunks) {
+    // faut changer l'url de temps en temps pcq elle est dynamique cheloue
+    const temp = await finalScraper({ chunk, page, path: "" }).catch((_) =>
+      exportToExcel(result)
+    );
+    result = result.concat(temp);
+  }
   exportToExcel(result);
   await browser.close();
+
+  // pour filter les noms qui existent au début
+  //getCleanPlantData();
 }
 
 main();
